1e7cebe3dc49c24e65788a09c40cda143ca664bf
Ted Dunn <tldunn451@gmail.com>
2017-02-21 08:19:56 -0500

    Add test set-up

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,6 +1,8 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
 
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 /**
@@ -8,6 +10,22 @@ import static org.junit.Assert.*;
  */
 public class UserTest {
 
-    
+    private Board scienceBoard;
+    private User Moe;
+    private User Larry;
+    private User Curly;
+    private Question question;
+    private Answer answer;
+
+    @Before
+    public void setUp() throws Exception {
+        scienceBoard = new Board("Great Scientific Minds");
+        Moe = scienceBoard.createUser("Moe");
+        Larry = scienceBoard.createUser("Larry");
+        Curly = scienceBoard.createUser("Curly");
+        question = Moe.askQuestion("Who invented calculus?");
+        answer = Larry.answerQuestion(question, "Isaac Newton");
+
+    }
 
 }
